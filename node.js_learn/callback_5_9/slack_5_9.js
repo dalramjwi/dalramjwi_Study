0900e7521381082dcb049b9018790e92ed63b8bd
SH <dalramjwi@gmail.com>
2024-05-12 01:06:45 +0900

    move to new folder

--- node.js_learn/callback_5_9/slack_5_9.js
+++
@@ -1,9 +0,0 @@
-const studentNamesArray = require("./names-data.js");
-//js 방식 - 선언형
-studentNamesArray.forEach((name) => {
-  console.log(name);
-});
-//일반 반복문 방식 -절차형
-for (let i = 0; i < studentNamesArray.length; i++) {
-  console.log(studentNamesArray[i]);
-}
--- node.js_learn/node.js_5_7/work.js
+++
@@ -1,3 +0,0 @@
-let name = 수현;
-
-console.log("hello" + name);
